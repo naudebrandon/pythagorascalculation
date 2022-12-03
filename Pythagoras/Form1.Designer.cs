8b316a06f9ffee5f51c51e73a755d0c2b4b21955
Brandon Naude <naudebrandon@gmail.com>
2022-12-03 11:22:03 +0200

    initial commit

---
+++ Pythagoras/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Pythagoras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape3 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radA = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.RadC = new System.Windows.Forms.RadioButton();
+            this.radB = new System.Windows.Forms.RadioButton();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1,
+            this.lineShape3,
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(364, 305);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 118;
+            this.lineShape1.X2 = 118;
+            this.lineShape1.Y1 = 14;
+            this.lineShape1.Y2 = 154;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 118;
+            this.lineShape2.X2 = 266;
+            this.lineShape2.Y1 = 154;
+            this.lineShape2.Y2 = 157;
+            // 
+            // lineShape3
+            // 
+            this.lineShape3.Name = "lineShape3";
+            this.lineShape3.X1 = 119;
+            this.lineShape3.X2 = 266;
+            this.lineShape3.Y1 = 15;
+            this.lineShape3.Y2 = 157;
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.Location = new System.Drawing.Point(118, 134);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(22, 20);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "c";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "b";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // radA
+            // 
+            this.radA.AutoSize = true;
+            this.radA.Location = new System.Drawing.Point(57, 230);
+            this.radA.Name = "radA";
+            this.radA.Size = new System.Drawing.Size(31, 17);
+            this.radA.TabIndex = 4;
+            this.radA.TabStop = true;
+            this.radA.Text = "a";
+            this.radA.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Solve";
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(57, 75);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(60, 20);
+            this.txtC.TabIndex = 6;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(174, 161);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(60, 20);
+            this.txtB.TabIndex = 7;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(207, 61);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(60, 20);
+            this.txtA.TabIndex = 8;
+            // 
+            // RadC
+            // 
+            this.RadC.AutoSize = true;
+            this.RadC.Location = new System.Drawing.Point(57, 276);
+            this.RadC.Name = "RadC";
+            this.RadC.Size = new System.Drawing.Size(31, 17);
+            this.RadC.TabIndex = 9;
+            this.RadC.TabStop = true;
+            this.RadC.Text = "c";
+            this.RadC.UseVisualStyleBackColor = true;
+            // 
+            // radB
+            // 
+            this.radB.AutoSize = true;
+            this.radB.Location = new System.Drawing.Point(57, 253);
+            this.radB.Name = "radB";
+            this.radB.Size = new System.Drawing.Size(31, 17);
+            this.radB.TabIndex = 10;
+            this.radB.TabStop = true;
+            this.radB.Text = "b";
+            this.radB.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(159, 220);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(154, 73);
+            this.btnCalculate.TabIndex = 11;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 305);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.radB);
+            this.Controls.Add(this.RadC);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Pythagoras";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape3;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.RadioButton RadC;
+        private System.Windows.Forms.RadioButton radB;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Pythagoras/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pythagoras
+{
+    public partial class Form1 : Form
+    {
+        Double a, b, c,answer;
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Double A=Convert.ToDouble(txtA.Text);
+            Double B =Convert.ToDouble (txtB.Text);
+            Double C =Convert.ToDouble (txtC.Text);
+
+            if (radA.Checked == true)
+            {
+                txtA.Clear();
+                txtA.Enabled = false;
+
+                answer = (((B*B) + (C*C));
+
+                txtA.Text=answer.ToString();
+
+            }
+
+        }
+    }
+}
